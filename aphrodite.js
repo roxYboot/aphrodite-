195e5736becd2b503b1ce877542c2423b4eaa3bc
roxYboot <43090878+roxYboot@users.noreply.github.com>
2018-09-08 22:45:16 +0300

    Update aphrodite.js

--- aphrodite.js
+++ aphrodite.js
@@ -233,7 +233,7 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
 
 
 client.on('message', function(message) {
-	const myID = "";
+	const myID = "308280511371935744";
     let args = message.content.split(" ").slice(1).join(" ");
     if(message.content.startsWith(prefix + "setname")) {
 		        if(message.author.id !== myID) return;
